59da7f5ce540aca65e7a44658e9a15f4b576cf80
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-05-18 18:29:08 +0800

    chore: remove sql first

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///weight.db"
 # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 #
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 
 # class WeightData(db.Model):
